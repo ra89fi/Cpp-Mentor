8add3ee3e1b9a8393da1062fb8290756f800063d
raihan <5421449+ra89fi@users.noreply.github.com>
2022-04-18 22:31:17 +0600

    blogs page

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const About = () => {
-    return <div>About Page</div>;
+    return (
+        <div style={{ padding: '0 2em 2em' }}>
+            <h4>This is About Page</h4>
+        </div>
+    );
 };
 
 export default About;
--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,7 +1,30 @@
 import React from 'react';
 
 const Blogs = () => {
-    return <div>Blogs Page</div>;
+    return (
+        <div style={{ padding: '0 2em 2em' }}>
+            <h4>Difference between authorization and authentication?</h4>
+            <p>
+                Authorization is checking if the user is allowed to do what he's
+                trying to do. <br />
+                Authentication is valiading an user himself.
+            </p>
+            <h4>
+                Why are you using firebase? What other options do you have to
+                implement authentication?
+            </h4>
+            <p>
+                Firebase gives us the tools necessary to implement an
+                authentication system without managing a lot of things ourselves
+                like user database etc.
+            </p>
+            <h4>
+                What other services does firebase provide other than
+                authentication
+            </h4>
+            <p>Hosting, Database etc.</p>
+        </div>
+    );
 };
 
 export default Blogs;
